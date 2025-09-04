4c3a0b106a49541a6b94b8087357d6a22f629e9b
Mingma Tenzing Sherpa <pasangsherpa689@gmail.com>
2025-09-04 10:17:26 +0800

    beans

---
+++ src/main/java/ming/springboot/Content_Calendar/Message.java
@@ -0,0 +1,10 @@
+package ming.springboot.Content_Calendar;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Message {
+    public String getMessage() {
+        return "Hello world";
+  }
+}
